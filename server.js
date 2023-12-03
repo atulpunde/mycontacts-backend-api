742ff9f412e2f82ce0dec1e23712d3b5ffa687b7
Atul Punde <atulpunde970@gmail.com>
2023-12-03 20:44:00 +0530

    Initial node and express app setup

---
+++ server.js
@@ -0,0 +1,14 @@
+// Importing Express framework into our Node.js application
+const express = require('express');
+require('dotenv').config()
+
+const app = express();
+
+// If you do "npm i dotenv" then you can set the "process.env.PORT", and use port from .env file
+const port = process.env.PORT || 5000;
+
+// Actual server started listning here.
+// Show the address and port in the command prompt or terminal
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+});
