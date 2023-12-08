f9bc6cfb2a27227ca470a3ad70d2c1cc564b3653
Atul Punde <atulpunde970@gmail.com>
2023-12-08 12:18:30 +0530

    recorrect initial requests

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -5,20 +5,24 @@ router.get('/', (req, res) => {
     res.status(200).json({"name":"get request"});
 });
 
+router.get('/:id', (req, res) => {
+    res.status(200).json({"name":`get request for id: ${req.params.id}`});
+});
+
 router.post('/', (req, res) => {
     res.status(201).json({"name":"post request"});
 });
 
-router.put('/', (req, res) => {
-    res.status(200).json({"name":"put request"});
+router.put('/:id', (req, res) => {
+    res.status(200).json({"name":`put request for id: ${req.params.id}`});
 });
 
-router.patch('/', (req, res) => {
-    res.status(200).json({"name":"patch request"});
+router.patch('/:id', (req, res) => {
+    res.status(200).json({"name":`patch request for id: ${req.params.id}`});
 });
 
-router.delete('/', (req, res) => {
-    res.status(200).json({"name":"delete request"});
+router.delete('/:id', (req, res) => {
+    res.status(200).json({"name":`delete request for id: ${req.params.id}`});
 });
 
 module.exports = router;
