9dc40330d3650c223769ba571a3fc92088f42b49
Atul Punde <atulpunde970@gmail.com>
2023-12-09 19:02:13 +0530

    added errorHandler middleware

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,43 +1,50 @@
+const errorHandler = require("../middleware/errorHandler");
+
 //@description Get all contacts
 //@route GET /api/contacts
 //@access public
-const getAllContacts = (req, res) => {
+const getAllContacts = errorHandler(async (req, res) => {
     res.status(200).json({"name":"get all request"});
-};
+});
 
 //@description Get contact
 //@route GET /api/contacts/:id
 //@access public
-const getContact = (req, res) => {
+const getContact = errorHandler(async (req, res) => {
     res.status(200).json({"name": `get contact for id: ${req.params.id}`});
-};
+});
 
 //@description Create new contact
 //@route POST /api/contacts
 //@access public
-const createContact = (req, res) => {
-    res.status(201).json({"name":"Create contact"});
-};
+const createContact = errorHandler(async (req, res) => {
+    const {name, email, phone} = req.body;
+    if(!name || !email || !phone) {
+        res.status(400);
+        throw new Error("All field are mandatory.");
+    }
+    res.status(201).json(req.body);
+});
 
 //@description Edit complete contact
 //@route PUT /api/contacts/:id
 //@access public
-const putContact = (req, res) => {
+const putContact = errorHandler(async (req, res) => {
     res.status(200).json({"name": `Edit complete contact for id: ${req.params.id}`});
-}
+})
 
 //@description Edit contact partially
 //@route PATCH /api/contacts/:id
 //@access public
-const patchContact = (req, res) => {
+const patchContact = errorHandler(async (req, res) => {
     res.status(200).json({"name":`Edit partial contact for id: ${req.params.id}`});
-}
+})
 
 //@description Delete contact
 //@route DELETE /api/contacts/:id
 //@access public
-const deleteContact = (req, res) => {
+const deleteContact = errorHandler(async (req, res) => {
     res.status(200).json({"name":`Delete contact for id: ${req.params.id}`});
-}
+})
 
 module.exports = { getAllContacts, getContact, createContact, putContact, patchContact, deleteContact };
--- server.js
+++ server.js
@@ -2,13 +2,20 @@
 const express = require('express');
 require('dotenv').config()
 const contactsRoutes = require('./routes/contactRoutes');
+const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
 // If you do "npm i dotenv" then you can set the "process.env.PORT", and use port from .env file
 const port = process.env.PORT || 5000;
 
+// When you need to accept data from client to a server
+app.use(express.json());
+
 app.use('/api/contacts', contactsRoutes);
+
+// created middleware to handle errors
+app.use(errorHandler);
 
 // Actual server started listning here.
 // Show the address and port in the command prompt or terminal
