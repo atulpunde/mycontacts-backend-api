a75fbef3404916877b6488c3117658057a5aa96c
Atul Punde <atulpunde970@gmail.com>
2023-12-10 00:19:18 +0530

    addressed issue: recorrected controller to use express-async-handler

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,23 +1,25 @@
-const errorHandler = require("../middleware/errorHandler");
+const asyncHandler = require('express-async-handler');
+const Contact = require("../models/contactModel");
 
 //@description Get all contacts
 //@route GET /api/contacts
 //@access public
-const getAllContacts = errorHandler(async (req, res) => {
+const getAllContacts = asyncHandler(async (req, res) => {
+    const contacts = Contact.find();
     res.status(200).json({"name":"get all request"});
 });
 
 //@description Get contact
 //@route GET /api/contacts/:id
 //@access public
-const getContact = errorHandler(async (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
     res.status(200).json({"name": `get contact for id: ${req.params.id}`});
 });
 
 //@description Create new contact
 //@route POST /api/contacts
 //@access public
-const createContact = errorHandler(async (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
     const {name, email, phone} = req.body;
     if(!name || !email || !phone) {
         res.status(400);
@@ -29,21 +31,21 @@ const createContact = errorHandler(async (req, res) => {
 //@description Edit complete contact
 //@route PUT /api/contacts/:id
 //@access public
-const putContact = errorHandler(async (req, res) => {
+const putContact = asyncHandler(async (req, res) => {
     res.status(200).json({"name": `Edit complete contact for id: ${req.params.id}`});
 })
 
 //@description Edit contact partially
 //@route PATCH /api/contacts/:id
 //@access public
-const patchContact = errorHandler(async (req, res) => {
+const patchContact = asyncHandler(async (req, res) => {
     res.status(200).json({"name":`Edit partial contact for id: ${req.params.id}`});
 })
 
 //@description Delete contact
 //@route DELETE /api/contacts/:id
 //@access public
-const deleteContact = errorHandler(async (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
     res.status(200).json({"name":`Delete contact for id: ${req.params.id}`});
 })
 
